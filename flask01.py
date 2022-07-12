801dc3c8978a8ba3e61b6c256ec92352faef57c3
Gwithjoy <bangbabo131@gmail.com>
2022-07-12 23:12:54 +0900

    third

--- flask01.py
+++ flask01.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Login</h1>'
+    return '<h1>Hello, World!</h1>'
 
 
-@app.route('/games')
-def reporter():
-    return '''<h2>games</h2>
-              <a href="/">logout</a>'''
+@app.route('/reporter/<int:reporter_id>')
+def reporter(reporter_id):
+    return f'''
+    <h2>Reporter {reporter_id} Bio</h2>
+    <a href="/">Return to home page</a>
+    '''
 
-
-if __name__ == "__main__":
-    app.run()
