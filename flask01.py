cf8b4d12d03678330e6f699f88f8409c34cf4cdc
Gwithjoy <bangbabo131@gmail.com>
2022-07-12 23:02:37 +0900

    second

--- flask01.py
+++ flask01.py
@@ -1,4 +1,19 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__)
-print(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return '<h1>Login</h1>'
+
+
+@app.route('/games')
+def reporter():
+    return '''<h2>games</h2>
+              <a href="/">logout</a>'''
+
+
+if __name__ == "__main__":
+    app.run()
